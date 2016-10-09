1e4513c2ac432975422e4460c38df2a248fd13d5
Hugo <hugo.encinas@bmtask.com>
2016-10-08 22:37:24 -0500

    contenido y estilos

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,7 @@
-console.log("test3");
+var piemenu = new wheelnav('piemenu');
+piemenu.spreaderInTitle = icon.plus;
+piemenu.spreaderOutTitle = icon.cross;
+piemenu.spreaderRadius = piemenu.wheelRadius * 0.34;
+piemenu.wheelRadius = piemenu.wheelRadius * 0.83;
+piemenu.createWheel();
+piemenu.setTooltips(['','','','','']);
--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,8 @@ gulp.task('browserSync', function() {
   browserSync({
     server: {
       baseDir: 'app'
-    }
+    },
+    port:80
   })
 })
 
@@ -37,7 +38,24 @@ gulp.task('sass', function() {
 
 gulp.task('hbs2html', function () {
     var templateData = {
-        firstName: 'Kaanon'
+        firstName: 'Gila',
+        lastName: 'De Lauren',
+        description: 'She is an Italian-born American chef, writer, television personality, and the host of the current Food Network television program Food Paradise',
+        dish:[
+          {src:'images/img-deviled-eggs.png', alt:'Pickled Shrimp', desc:"Bar American's Pickled Shrimp Deviled Eggs with Cornichon Remoulade"},
+          {src:'images/img-roulade-cover.png', alt:'Turkey Roulade', desc:'Turkey Roulade with "Stovetop" Stuffing'},
+          {src:'images/img-fresh-tomato-sauce-cover.png', alt:'Pasta', desc:'Pasta with Fresh Tomato Sauce'}
+        ],
+        special:[
+          {title:'Barbeque: Cali Style', src:'images/img-the-ingredients-cover.png', alt:'Ingredients', desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In viverra molestie purus in vehicula. Aliquam erat volutpat. Praesent fringilla lorem at nisi venenatis egestas quis non nulla. Integer blandit tempus fermentum. Phasellus id congue eros. Vestibulum dapibus tristique libero, quis mattis risus volutpat a. Quisque efficitur ligula diam, vitae viverra arcu consectetur vel. Vivamus nunc augue, pharetra vel augue sed, vestibulum vulputate urna. Maecenas aliquet sed lacus quis semper. Nullam convallis enim ac convallis consectetur. Maecenas id laoreet lacus. Donec cursus nulla eget tincidunt faucibus.'},
+          {title:'The Perfect Fruitcake', src:'images/img-fruitcake-cover.png', alt:'Fruitcake', desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In viverra molestie purus in vehicula. Aliquam erat volutpat. Praesent fringilla lorem at nisi venenatis egestas quis non nulla. Integer blandit tempus fermentum. Phasellus id congue eros. Vestibulum dapibus tristique libero, quis mattis risus volutpat a. Quisque efficitur ligula diam, vitae viverra arcu consectetur vel. Vivamus nunc augue, pharetra vel augue sed, vestibulum vulputate urna. Maecenas aliquet sed lacus quis semper. Nullam convallis enim ac convallis consectetur. Maecenas id laoreet lacus. Donec cursus nulla eget tincidunt faucibus.'},
+          {title:"Spice-Crusted Salmon: A Holiday Dinner That's As Impressive As It Is Quick", src:'images/img-salmon-cover.png', alt:'Salmon', desc:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In viverra molestie purus in vehicula. Aliquam erat volutpat. Praesent fringilla lorem at nisi venenatis egestas quis non nulla. Integer blandit tempus fermentum. Phasellus id congue eros. Vestibulum dapibus tristique libero, quis mattis risus volutpat a. Quisque efficitur ligula diam, vitae viverra arcu consectetur vel. Vivamus nunc augue, pharetra vel augue sed, vestibulum vulputate urna. Maecenas aliquet sed lacus quis semper. Nullam convallis enim ac convallis consectetur. Maecenas id laoreet lacus. Donec cursus nulla eget tincidunt faucibus.'}
+        ],
+        restaurant:[
+          {title:'Deli Bar', src:'images/img-restaurante-1.png', alt:'Deli Bar', street:'156 Lafayette Street', city:'New York, NY 10012', number:'212 667 6400', website:'delibar.com'},
+          {title:'Bar New York', src:'images/img-restaurant-2.png', alt:'Bar New York', street:'756 W. 52nd Street', city:'New York, NY 10012', number:'214 265 8900', website:'bar-new-york.com'},
+          {title:'Bar Tokio Sun', src:'images/img-restaurant-3.png', alt:'Bar Tokio Sun', street:'11 Mohegan Sun Boulevard', city:'Uncasville, CT 06382', number:'756 862 3529', website:'tokisun.com'}
+        ]
     },
     options = {
         ignorePartials: true, //ignores the unknown footer2 partial in the handlebars template, defaults to false
@@ -45,6 +63,9 @@ gulp.task('hbs2html', function () {
         helpers : {
             capitals : function(str){
                 return str.toUpperCase();
+            },
+            fullName:function(){
+                return templateData.firstName+" "+templateData.lastName;
             }
         }
     }
